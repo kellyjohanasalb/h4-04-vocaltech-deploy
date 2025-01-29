42a8b6f23e0d0886d6170380a7039d4ee3dcd864
Anthony Mendoza <goodfriend_18@hotnail.com>
2025-01-29 14:31:14 -0500

    creacion de adminpanel

--- client/src/Pages/App/index.jsx
+++ client/src/Pages/App/index.jsx
@@ -1,10 +1,12 @@
 import { useRoutes, BrowserRouter,Navigate } from 'react-router-dom'
 import Login from '../Login'
 import {Registronuevo} from"../../modules/auth/pages/Registronuevo"
+import { Adminpage } from '../../modules/Panel Admin/pages/Adminpage'
 const AppRoutes = () => {
   let routes = useRoutes([
     {path: '/', element: <Login /> },
     {path: '/registro', element: <Registronuevo /> },
+    {path:"/Paneladmin", element:<Adminpage/>},
     {path: "*",
       element: <Navigate to="/" replace />,}
     
---
+++ client/src/modules/Panel Admin/pages/Adminpage.jsx
@@ -0,0 +1,5 @@
+import Dashboard from "../components/Paneladmin"
+
+export const Adminpage = () => {
+    return <Dashboard />;
+  };
