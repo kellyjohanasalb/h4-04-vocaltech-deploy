2418bae276bdf07e1d6742b074a3eac9bb6e70eb
ValeMP12 <preciadovaleriam@gmail.com>
2025-02-07 14:30:03 -0600

    Correccion sobre el boton regresar

--- src/components/Pitch2.jsx
+++ src/components/Pitch2.jsx
@@ -1,6 +1,7 @@
 import { CgArrowRight } from "react-icons/cg";
-import Header from "./Header";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ProgresBar from "./ProgresBar";
 
 
 
@@ -18,6 +19,10 @@ export default function Pitch2() {
             [name]: value,
         });
     };
+    const navigate = useNavigate(); // Hook para la navegación
+        const handleBackClick = () => {
+            navigate("/pitch1");
+        };
     const handleSubmit = (e) => {
         e.preventDefault();
         // Validar que todos los campos están llenos
@@ -34,7 +39,34 @@ export default function Pitch2() {
     return (
         <div className="flex flex-col items-center min-h-screen py-8 bg-white">
             {/* Header */}
-            <Header />
+            <div>
+            <header className="flex items-center justify-center w-full max-w-4xl px-4">
+                <button onClick={handleBackClick} className="absolute flex items-center gap-4 text-lg font-medium text-black lg:left-36 max-lg:left-72">
+                    <svg
+                        width="26"
+                        height="16"
+                        viewBox="0 0 26 16"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M0.292892 7.29289C-0.0976315 7.68342 -0.0976315 8.31658 0.292892 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292892 7.29289ZM26 7L1 7V9L26 9V7Z"
+                            fill="black"
+                        />
+                    </svg>
+                    Regresar
+                </button>
+                <img
+                    src="/VocalTech.png"
+                    alt="VocalTech"
+                    className="lg:w-[328px] h-[54px]"
+                />
+            </header>
+            {/* Progress Bar */}
+            <div className="flex items-center justify-between w-full max-w-4xl mt-8">
+                <ProgresBar />
+            </div>
+        </div>
             {/* Form */}
             <form className="w-full max-w-4xl px-4 mt-8" onSubmit={handleSubmit}>
                 <div>
