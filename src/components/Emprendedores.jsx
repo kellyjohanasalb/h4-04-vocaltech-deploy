853cb1577354c18ee30e2b352a16d5d547aede87
ValeMP12 <preciadovaleriam@gmail.com>
2025-02-11 18:25:00 -0600

    Terminada la parte de emprendedores

--- src/components/Emprendedores.jsx
+++ src/components/Emprendedores.jsx
@@ -1,7 +1,24 @@
+import BeneficiosEmpre from "./BeneficiosEmpre";
+import Clientes from "./Clientes";
+import ComoFunciona from "./ComoFunciona";
+import Footer from "./footer";
+import HeroEmprendimiento from "./HeroEmprendimiento";
+import Navbar from "./Navbar";
 
 
 export default function Emprendedores() {
     return (
-        <div>Emprendedores</div>
+        <div>
+            <div className="mx-auto">
+            <header>
+                <Navbar />
+            </header>
+            <HeroEmprendimiento />
+            <BeneficiosEmpre />
+            <ComoFunciona />
+            <Clientes />
+            <Footer />
+        </div>
+        </div>
     )
 }
